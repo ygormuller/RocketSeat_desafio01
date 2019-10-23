a6db3452a88eebb6c8abac289724b4a718e0960f
Ygor <yrmuller@yahoo.com.br>
2019-10-23 00:04:35 -0300

    inicio

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+const projects = [];
+
+//Middleware global para contagem de requisições
+server.use((req, res, next) => {
+  
+  console.count("Number of requests");
+  
+  return next();
+})
+
+//Middleware para verificar id de projetos
+
+function idCheckExists (req, res, next) {
+  const { id } = req.params;
+  const checkId = projects.find(proj=>proj.id == id);
+  if(!checkId){
+    return res.status(400).json({error: 'id not exists'});
+  }
+  req.checkId = checkId;
+  return next();
+ };
+
+
+//Todos os projetos criados
+server.get('/projects', (req, res) =>{
+  return res.json(projects);
+})
+
+//Buscando projeto pelo id
+server.get ('/projects/:id', (req, res)=> {
+  const { id } = req.params;
+  return res.json(projects[id]);
+})
+
+//Criando projeto
+server.post ('/projects', (req, res)=> {
+  const { project } = req.body;
+  projects.push(project);
+  return res.json(projects);
+})
+
+//Editando titulo do projeto
+server.put('/projects/:id', idCheckExists, (req, res)=>{
+  const { id } = req.params;
+  const { title } = req.body;
+  const checkId = projects.find(proj=>proj.id == id);
+  projects[id].title = title;
+  return res.json(projects);  
+})
+
+//Deletar projeto
+server.delete('/projects/:id', (req, res)=>{
+  const { id } = req.params;
+  projects.splice(id, 1);
+  //return res.json(projects);//retona a lista
+  return res.send();//não retorna lista
+});
+
+server.listen(3000);
+
